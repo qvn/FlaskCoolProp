ffdeb5b0271bac5388f90d09580f6b997a6c06a9
Quyen Nguyen <quyenvunguyen@gmail.com>
2014-05-18 18:59:14 -0500

    changed template to include FluidList

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import Form
-from wtforms import TextField, validators, DecimalField
+from wtforms import TextField, validators, DecimalField, SelectField
 from wtforms.validators import DataRequired
 
 class PressureForm(Form):
+	Fluid = SelectField('Fluid', choices=[('Water', 'Water'), ('Pentane', 'Pentane'), ('Propane', 'Propane')])
 	Pressure = DecimalField('Pressure', validators = [DataRequired()])
 	Temperature = DecimalField('Temperature',validators = [DataRequired()])
 
